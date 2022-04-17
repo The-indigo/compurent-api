c359a715b9f4ccd1a18072237b66c157d61c014b
The-indigo <adepojuadeyemi11@gmail.com>
2022-04-16 22:23:04 -0400

    delete a basket

--- routes/basket.js
+++ routes/basket.js
@@ -7,6 +7,7 @@ let basketController = require('../controllers/basket.controller');
 router.get('/',passport.authenticate('jwt', {session: false}), basketController.viewBasket);
 router.get('/all',basketController.viewAllBasket);
 router.post('/add/:deviceId', passport.authenticate('jwt', { session: false }), basketController.addToBasket);
+router.get('/delete/:id',passport.authenticate('jwt', { session: false }), basketController.deleteBasketById);
 router.delete('/', basketController.deleteBasket);
 
 
